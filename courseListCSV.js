e3264c2b563800d0b9e75bbf8fdcc764a53e343c
Erin McNulty <emcnulty19@lawrenceville.org>
2019-09-30 20:43:40 -0400

    Add files via upload

---
+++ courseListCSV.js
@@ -0,0 +1,347 @@
+var courseList = `veracross_course_id,Subject,Course ID,Course Name,veracross_grading_period_id,Rising form
+956,English,EN301,English III,50,3
+1258,English,EN421,The Novel,2,4
+1259,English,EN422,Advanced Poetry,4,4
+1260,English,EN423,Essay Writing,6,4
+965,English,EN501,Writing Seminar,2,5
+967,English,EN503,Advanced Fiction Seminar,6,5
+1707,English,EN504,Literature Seminar,4,5
+968,English,EN505,Style And Expression In Creative Writing,6,5
+969,English,EN506,Literary Journalism,4,5
+970,English,EN507,Literature Of National Pastime,2,5
+971,English,EN508,African American Literature,2,5
+1779,English,EN509,Advanced Literary Research,2,5
+972,English,EN510,Literature Of The Beat Generation,4,5
+1268,English,EN513,Nobel Pulitzer National Booker,2,5
+1690,English,EN514,American Indian Literatures,4,5
+1784,English,EN515,From Pulp to Pop: Literature Outside the Canon,4,5
+976,English,EN516,Essays Of Reflection,6,5
+1785,English,EN517,The Waste Land,4,5
+1619,English,EN518,Literature as Philosophy,6,5
+1780,English,EN520,From Freud To The Void,2,5
+1782,English,EN521,Gothic Literature,2,5
+1710,English,EN522,Inventing Truth: The Art of Memoir,6,5
+979,English,EN523,Hemingway,4,5
+926,English,EN525,Irish Literature,6,5
+982,English,EN528,Law As Literature,6,5
+986,English,EN533,Moby Dick,2,5
+1271,English,EN536,Creative Non-Fiction,4,5
+990,English,EN540,Poetry Seminar,4,5
+994,English,EN542,Southern Literature,2,5
+996,English,EN544,Shakespeare's Comedies: Masks We Wear,2,5
+1276,English,EN547,The Screwball Comedy,4,5
+998,English,EN548,West Of Everything,2,5
+1781,English,EN549,Linguistics: Dr. Johnson To Dr. Chomsky,4,5
+1001,English,EN551,Signifying Nothing: Experimental Novels,6,5
+1643,English,EN552,Children's Literature,6,5
+1277,English,EN554,Chaucer: Text and Context,4,5
+1003,English,EN555,Boarding School Literature,6,5
+1004,English,EN556,Modern English Plays,6,5
+1717,English,EN557,Electric Century: Literature Under Victoria,6,5
+1017,English,EN559,The Graphic Novel,6,5
+378,English,EN561,Storytelling in Literature and Film,2,5
+1718,English,EN562,American Landscapes,6,5
+1008,English,EN563,American Literature and the Free Market,6,5
+1009,English,EN564,Mystery of Language: Detective Fiction,4,5
+1010,English,EN565,War Stories,4,5
+1783,English,EN568,Found in Translation,4,5
+1652,English,EN569,Gender and the Body in Literature,6,5
+1638,English,EN573,Grit Lit: Readings of Perseverance and Hope,2,5
+1720,English,EN578,Dystopian Literature and Social Protest,4,5
+1721,English,EN579,"Literature, Trauma and Resistance",2,5
+1015,English,EN599,Ind. Study: English,2,5
+1015,English,EN599,Ind. Study: English,4,5
+1015,English,EN599,Ind. Study: English,6,5
+1016,English,HU201,Humanities - English,50,2
+1018,History,HI301,Forces That Shaped The Modern World,50,3
+1595,History,HI421,Themes in U.S. History,2,4
+1596,History,HI422,Themes in U.S. History,4,4
+1597,History,HI423,Themes in U.S. History,6,4
+1749,History,HI433,Fact or Fiction? Understanding History Through Film and Literature,6,45
+1021,History,HI451,Economics,2,45
+1021,History,HI451,Economics,4,45
+1755,History,HI508,Modern Latin America: Continuities and Change,2,45
+1744,History,HI509,"Racism, Resistance, and Reconciliation: Stories of South Africa's 20th Century",6,45
+991,History,HI511,Honors U.S. History,2,45
+1309,History,HI512,Honors U.S. History,4,45
+1310,History,HI513,Honors U.S. History,6,45
+992,History,HI515,Advanced Research Seminar,2,5
+1026,History,HI521,Honors Government,2,5
+1311,History,HI522,Honors Government,4,5
+1312,History,HI523,Honors Government,6,5
+1702,History,HI526,Race and Mass Incarceration: A Historical Perspective,2,5
+1027,History,HI527,Lincoln,2,5
+1027,History,HI527,Lincoln,4,5
+1778,History,HI528,Civil Rights and the American Constitution,6,5
+1028,History,HI530,European History: The Renaissance,2,45
+1029,History,HI531,European History: Revolutions,4,45
+1708,History,HI533,Origins of the Totalitarian State,6,45
+1801,History,HI537,Cases in American Democracy,4,5
+1802,History,HI538,Cases in American Democracy,6,5
+1031,History,HI551,Honors Economics,2,5
+1313,History,HI552,Honors Economics,4,5
+1314,History,HI553,Honors Economics,6,5
+1633,History,HI556,Public Policy Economics,4,45
+1645,History,HI557,Financial Crisis,6,45
+1036,History,HI575,The American Presidency,2,5
+1756,History,HI579,Civil War and Reconstruction,4,5
+420,History,HI580,Canadian Studies,4,45
+1703,History,HI584,The Athenian Century - The Rise and Fall of Athens,2,5
+1704,History,HI585,Topics in the History of Women and Gender,6,45
+1044,History,HI587,Reporting Vietnam,6,5
+1046,History,HI599,Ind. Study: History,2,5
+1046,History,HI599,Ind. Study: History,4,5
+1046,History,HI599,Ind. Study: History,6,5
+954,History,HU204,Humanities - Cultural Studies,50,2
+964,Interdisciplinary,IN504,Legal Practice And Procedures,2,5
+1748,Interdisciplinary,IN506,Forms We Live By,6,5
+1640,Interdisciplinary,IN507,Spies...Like us: Cold War Literature,4,5
+1786,Interdisciplinary,IN508,Science and Literature,2,5
+1266,Interdisciplinary,IN509,Science and Literature,4,5
+1302,Interdisciplinary,IN511,Modern Thought And Literature,6,5
+1013,Interdisciplinary,IN512,Heuristics,6,5
+1654,Interdisciplinary,IN513,Poverty,46,5
+1795,Interdisciplinary,IN515,A Hero's Journey: Lewis and Clark ,46,45
+1025,Interdisciplinary,IN516,Russia: History and Memory,4,45
+1648,Interdisciplinary,IN519,Brazil: Order and Progress,4,5
+1033,Interdisciplinary,IN520,Africa: Then and Now,24,5
+1039,Interdisciplinary,IN522,The Middle East - Myth And History,2,45
+1039,Interdisciplinary,IN522,The Middle East - Myth And History,4,45
+1743,Interdisciplinary,IN523,Civil Liberties and the American Constitution,2,5
+1045,Interdisciplinary,IN524,Immigration Stories/Theories,24,5
+1696,Interdisciplinary,IN525,Honors French: French Cinema,6,345
+1804,Interdisciplinary,IN527,Honors Spanish:  The Hispanic History of the U.S.,6,345
+1088,Interdisciplinary,IN528,Honors Spanish:  Politics & Poetry,4,345
+1106,Interdisciplinary,IN530,Honors Calculus with Physics,50,45
+1178,Interdisciplinary,IN531,Honors Physics With Calculus,50,45
+1733,Interdisciplinary,IN533,"Hamilton, the Musical:  History, Hip-Hop & Telling a Story",4,45
+1733,Interdisciplinary,IN533,"Hamilton, the Musical:  History, Hip-Hop & Telling a Story",6,45
+1647,Interdisciplinary,IN535,"Mythological Theatre: Gods, Heroes, and Monsters",6,45
+1692,Interdisciplinary,IN536,The Exploration of the Universe through Theatre,2,45
+1798,Interdisciplinary,IN537,"The Culture Industry in the US:  Media, Marketing, and Music",2,45
+1140,Interdisciplinary,IN538,Varieties of Religious Experience,4,45
+1152,Interdisciplinary,IN542,The Karma Of Words,4,45
+1153,Interdisciplinary,IN544,Religious Dimensions Of Music,2,45
+1154,Interdisciplinary,IN546,Religion And Ecology,6,45
+1157,Interdisciplinary,IN550,Makers Modern Mind,46,45
+1181,Interdisciplinary,IN558,Bioethics,46,5
+1183,Interdisciplinary,IN560,Race,6,45
+1186,Interdisciplinary,IN562,Food Studies,6,45
+1745,Interdisciplinary,IN564,"Honors Art History:Athens, Rome, Constantinople/Istanbul",2,45
+1746,Interdisciplinary,IN565,Honors Art History:The High Renaissance-Genius at Work,4,45
+1747,Interdisciplinary,IN566,Honors Art History:Cogito Ergo Sum,6,45
+1205,Interdisciplinary,IN568,Design For Social Change,2,5
+1211,Interdisciplinary,IN584,Urban Education in America,6,5
+1043,Interdisciplinary,IN588,Capstone,6,5
+1676,Interdisciplinary,IN599,Ind. Study: Interdisciplinary,2,5
+1676,Interdisciplinary,IN599,Ind. Study: Interdisciplinary,4,5
+1676,Interdisciplinary,IN599,Ind. Study: Interdisciplinary,6,5
+1048,Language,LA201,French 1,50,2
+1049,Language,LA211,Spanish 1,50,2
+1050,Language,LA221,Chinese 1,50,2
+1051,Language,LA241,Latin 1,50,2
+1052,Language,LA301,French 2,50,234
+1053,Language,LA311,Spanish 2,50,234
+1054,Language,LA321,Chinese 2,50,324
+1055,Language,LA341,Latin 2,50,234
+1056,Language,LA401,French 3,2,2345
+1315,Language,LA402,French 3,4,2345
+1316,Language,LA403,French 3,6,2345
+1059,Language,LA411,Spanish 3,2,2345
+1319,Language,LA412,Spanish 3,4,2345
+1320,Language,LA413,Spanish 3,6,2345
+1060,Language,LA421,Chinese 3,2,2345
+1321,Language,LA422,Chinese 3,4,2345
+1322,Language,LA423,Chinese 3,6,2345
+1061,Language,LA441,Latin 3,2,2345
+1323,Language,LA442,Latin 3,4,2345
+1324,Language,LA443,Latin 3,6,2345
+1655,Language,LA501,Advanced Latin I,2,2345
+1656,Language,LA502,Advanced Latin II,4,2345
+1657,Language,LA503,Advanced Latin III,6,2345
+1658,Language,LA504,Advanced Chinese I,2,2345
+1659,Language,LA505,Advanced Chinese II,4,2345
+1660,Language,LA506,Advanced Chinese III,6,2345
+1661,Language,LA507,Advanced French I,2,2345
+1662,Language,LA508,Advanced French II,4,2345
+1663,Language,LA509,Advanced French III,6,2345
+1664,Language,LA510,Advanced Spanish I,2,2345
+1665,Language,LA511,Advanced Spanish II,4,2345
+1666,Language,LA512,Advanced Spanish III,6,2345
+1653,Language,LA515,Introduction to Ancient Greek,50,2345
+1706,Language,LA521,Honors Chinese: Topics in Modern China I,2,2345
+1613,Language,LA522,Honors Chinese: Topics in Modern China II,4,2345
+1614,Language,LA523,Honors Chinese: Topics in Modern China III,6,2345
+1074,Language,LA527,Honors Chinese: Journalism I,2,2345
+1075,Language,LA528,Honors Chinese: Journalism II,4,2345
+1076,Language,LA529,Honors Chinese: Journalism III,6,2345
+1711,Language,LA545,Honors Latin: Roman Historiography,2,345
+1712,Language,LA546,Honors Latin: Roman Theater,4,345
+1793,Language,LA547,Honors Latin: Ancient Rome & the Cinema,6,345
+1081,Language,LA564,Honors French: Paris through the Ages,4,345
+1083,Language,LA566,Honors French: The Ninth Art,2,345
+1713,Language,LA585,Honors Spanish: Contemporary Themes and Expression,4,345
+1734,Language,LA593,Honors Spanish: Muralists of Mexico,6,345
+1089,Language,LA594,Honors Spanish: Improvisaciones,6,345
+1090,Language,LA595,Honors Spanish: Fiction And Reality,2,345
+1092,Language,LA597,Honors Spanish: News and Culture,2,345
+1093,Language,LA599,Ind. Study: Language,2,5
+1093,Language,LA599,Ind. Study: Language,4,5
+1093,Language,LA599,Ind. Study: Language,6,5
+1094,Mathematics,MA201,Math 1,50,2
+1095,Mathematics,MA204,Math 2,50,23
+1097,Mathematics,MA301,Math 3,50,234
+1787,Mathematics,MA321,Math 3X,50,234
+1098,Mathematics,MA404,Math 4,50,345
+1099,Mathematics,MA407,Precalculus,50,345
+1100,Mathematics,MA411,Statistics,50,345
+1101,Mathematics,MA421,Precalculus BC,50,345
+1788,Mathematics,MA451,Calculus,50,345
+1105,Mathematics,MA504,Honors Calculus AB,50,2345
+1107,Mathematics,MA511,Honors Statistics,50,345
+1108,Mathematics,MA521,Honors Calculus BC,50,2345
+1109,Mathematics,MA527,Honors Calculus-Based Probability And Statistics,50,345
+1789,Mathematics,MA532,Honors Math Seminar: Linear Algebra,2,45
+1111,Mathematics,MA536,Honors Math Seminar: Multivariable Calculus,4,45
+1112,Mathematics,MA537,Honors Math Seminar: Differential Equations,6,45
+1115,Mathematics,MA551,Statistical Reasoning In Sports,4,45
+1289,Mathematics,MA555,Honors Computer Programming,50,45
+1790,Mathematics,MA561,Javascript Programming,2,45
+1791,Mathematics,MA562,Python Programming,4,45
+1792,Mathematics,MA563,Projects in Computer Programming,6,45
+1116,Mathematics,MA599,Ind. Study: Math,2,5
+1116,Mathematics,MA599,Ind. Study: Math,4,5
+1116,Mathematics,MA599,Ind. Study: Math,6,5
+617,Non Academic,LW205,Hutchins Scholars,2,45
+1674,Non Academic,LW206,Hutchins Scholars,4,4
+1675,Non Academic,LW207,Hutchins Scholars,6,4
+674,Performing Arts,PA010,The Lawrentians,50,45
+1117,Performing Arts,PA201,Foundations of Music,46,2
+1118,Performing Arts,PA221,Foundations of Theater,46,2
+1119,Performing Arts,PA301,Foundations of Music,2,3
+1113,Performing Arts,PA304,The Lawrentians,50,3
+1120,Performing Arts,PA310,Explorations of Music,24,3
+1121,Performing Arts,PA321,Foundations of Theatre,6,3
+1122,Performing Arts,PA322,Acting & Directing,2,3
+1122,Performing Arts,PA322,Acting & Directing,6,3
+1124,Performing Arts,PA324,Explorations of Theatre,24,3
+1803,Performing Arts,PA325,Stagecraft and Design,2,3
+1125,Performing Arts,PA330,Shapes and Styles of Music,2,3
+1125,Performing Arts,PA330,Shapes and Styles of Music,4,3
+1615,Performing Arts,PA421,Fundamentals of Theatre,6,4
+1693,Performing Arts,PA511,Honors Music Theory: Basics of Structure,2,345
+1694,Performing Arts,PA512,Honors Music Theory: Functional Harmony,4,345
+1695,Performing Arts,PA513,Honors Music Theory: Form and Analysis,6,345
+1794,Performing Arts,PA515,Jazz Improvisation,2,345
+1138,Performing Arts,PA599,Ind. Study: Music or Theatre,2,5
+1138,Performing Arts,PA599,Ind. Study: Music or Theatre,4,5
+1138,Performing Arts,PA599,Ind. Study: Music or Theatre,6,5
+1689,Religion and Philosophy,RP100,Religious Studies for Lawrentians,50,4
+1714,Religion and Philosophy,RP302,Introduction to Religious Studies,2,3
+1714,Religion and Philosophy,RP302,Introduction to Religious Studies,4,3
+1714,Religion and Philosophy,RP302,Introduction to Religious Studies,6,3
+1141,Religion and Philosophy,RP402,Social Ethics And Genocide,4,345
+1143,Religion and Philosophy,RP407,Studies In Christian Origins,6,345
+1145,Religion and Philosophy,RP409,Judaism,6,345
+1148,Religion and Philosophy,RP412,Islamic Studies,6,345
+1149,Religion and Philosophy,RP413,Ethics,4,345
+1149,Religion and Philosophy,RP413,Ethics,6,345
+1150,Religion and Philosophy,RP414,Philosophy,2,45
+1650,Religion and Philosophy,RP420,Buddhist Studies,6,345
+1160,Religion and Philosophy,RP515,Jerusalem,2,45
+1691,Religion and Philosophy,RP522,Topics in Religion: Hindu Myth in the Popular Culture,4,45
+1751,Religion and Philosophy,RP523,Topics in Religion: Religion and Violence,2,45
+1649,Religion and Philosophy,RP530,Applied Ethics,2,45
+1161,Religion and Philosophy,RP599,Ind Study: Religion & Philosophy,2,5
+1161,Religion and Philosophy,RP599,Ind Study: Religion & Philosophy,4,5
+1161,Religion and Philosophy,RP599,Ind Study: Religion & Philosophy,6,5
+1610,Science,SC205,Inquiries in Biological and Environmental Sciences,50,2
+1611,Science,SC325,Inquiries in Chemical and Physical Sciences,50,3
+1166,Science,SC413,Evolution Of Reproduction,4,45
+1167,Science,SC414,Human Disease,6,45
+572,Science,SC415,Human Anatomy And Physiology,6,45
+1697,Science,SC421,Forensic Science,2,45
+1797,Science,SC422,Extensions in Forensic Science,4,45
+1168,Science,SC431,Conceptual Physics,2,45
+1213,Science,SC432,Conceptual Physics,4,45
+575,Science,SC433,Conceptual Physics,6,45
+1170,Science,SC437,Astronomy,6,45
+1738,Science,SC441,Chemistry,2,45
+1739,Science,SC442,Chemistry,4,45
+1740,Science,SC443,Chemistry,6,45
+1752,Science,SC451,Biology,2,45
+1753,Science,SC452,Biology,4,45
+1754,Science,SC453,Biology,6,45
+1796,Science,SC462,Environmental Science,46,45
+1736,Science,SC507,Human Evolution,2,45
+1737,Science,SC508,The Biological Roots Of Human Behavior,4,45
+1172,Science,SC511,Honors Ecology,2,45
+1698,Science,SC512,Honors Ecology: Field Botany,2,45
+1174,Science,SC516,Honors Biology,2,45
+1327,Science,SC517,Honors Biology,4,45
+1328,Science,SC518,Honors Biology,6,45
+1175,Science,SC519,Research In Field Ecology,46,45
+1176,Science,SC522,Honors Chemistry,2,45
+1329,Science,SC523,Honors Chemistry,4,45
+1330,Science,SC524,Honors Chemistry,6,45
+1177,Science,SC526,Research In Applied Chemistry,24,45
+1209,Science,SC528,Biochemistry,2,45
+1601,Science,SC531,Physics,2,45
+1602,Science,SC532,Physics,4,45
+1603,Science,SC533,Physics,6,45
+1604,Science,SC544,Physics / Mechanics,2,45
+1605,Science,SC545,Physics / Mechanics,4,45
+1606,Science,SC546,Physics / Mechanics,6,45
+1607,Science,SC557,Honors Physics / Mechanics,2,45
+1608,Science,SC558,Honors Physics / Mechanics,4,45
+1609,Science,SC559,Honors Physics / Mechanics,6,45
+1305,Science,SC562,Intro to Engineering,4,45
+1646,Science,SC563,Projects in Engineering,6,45
+1699,Science,SC571,Research and Social Justice,2,5
+1182,Science,SC573,Sustainability Seminar,2,45
+1741,Science,SC574,Research in Molecular Genetics,24,5
+1750,Science,SC575,Projects in Molecular Genetics,6,5
+1184,Science,SC584,Honors Environmental Science,2,45
+1335,Science,SC585,Honors Environmental Science,4,45
+1336,Science,SC586,Honors Environmental Science,6,45
+1185,Science,SC590,Learning And Memory,4,45
+611,Science,SC599,Ind. Study: Science,2,5
+611,Science,SC599,Ind. Study: Science,4,5
+611,Science,SC599,Ind. Study: Science,6,5
+1188,Visual Arts,VA200,Foundations Of Art I,46,2
+1189,Visual Arts,VA300,Foundations Of Art,2,3
+1190,Visual Arts,VA301,Drawing,2,3
+1190,Visual Arts,VA301,Drawing,4,3
+1190,Visual Arts,VA301,Drawing,6,3
+1672,Visual Arts,VA303,Video Journalism,2,3
+1672,Visual Arts,VA303,Video Journalism,4,3
+1672,Visual Arts,VA303,Video Journalism,6,3
+1799,Visual Arts,VA304,Technical Design & Digital Fabrication,2,3
+1799,Visual Arts,VA304,Technical Design & Digital Fabrication,4,3
+1799,Visual Arts,VA304,Technical Design & Digital Fabrication,6,3
+1192,Visual Arts,VA401,Painting,2,345
+1192,Visual Arts,VA401,Painting,4,435
+1192,Visual Arts,VA401,Painting,6,345
+1194,Visual Arts,VA410,Architecture,6,45
+1195,Visual Arts,VA412,Ceramics I,2,45
+1195,Visual Arts,VA412,Ceramics I,4,45
+1195,Visual Arts,VA412,Ceramics I,6,45
+1196,Visual Arts,VA414,Photography I,2,45
+1196,Visual Arts,VA414,Photography I,4,45
+1196,Visual Arts,VA414,Photography I,6,45
+1199,Visual Arts,VA422,Ceramics II,6,45
+1200,Visual Arts,VA424,Photography II,6,45
+1303,Visual Arts,VA430,Filmmaking: Sight & Sound,6,45
+1202,Visual Arts,VA501,Advanced Studio,50,45
+1800,Visual Arts,VA520,Advanced Technical Design & Digital Fabrication,2,45
+1800,Visual Arts,VA520,Advanced Technical Design & Digital Fabrication,4,45
+1800,Visual Arts,VA520,Advanced Technical Design & Digital Fabrication,6,45
+1206,Visual Arts,VA599,Ind. Study: Visual Arts,2,5
+1206,Visual Arts,VA599,Ind. Study: Visual Arts,4,5
+1206,Visual Arts,VA599,Ind. Study: Visual Arts,6,5
+955,Additional Academic Programs ,LW101,Term Away,2,45
+955,Additional Academic Programs ,LW101,Term Away,4,45
+955,Additional Academic Programs ,LW101,Term Away,6,45
+616,Additional Academic Programs ,LW102,Island School,2,34
+616,Additional Academic Programs ,LW102,Island School,4,34
+616,Additional Academic Programs ,LW102,Island School,6,34`
